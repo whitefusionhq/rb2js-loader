fd599231436daf5abfb35046d0120d475735e1c2
Jared White <jared@jaredwhite.com>
2020-10-09 23:48:12 -0700

    fix: Ruby2JS errors are now re-thrown

--- src/index.js
+++ src/index.js
@@ -3,5 +3,9 @@ import { spawnSync } from "child_process"
 export default function loader(source) {
   const result = spawnSync('bundle', ['exec', 'ruby', '-e require "./rb2js.config.rb"; puts Ruby2JS::Loader.process(ARGF.read)'], { input: source });
 
+  if (result.stderr.length > 0) {
+    this.emitError(new Error(result.stderr))
+  }
+
   return result.stdout;
 }
